48d3b2c370e9a430e276c1edacf80a7bf82941fb
Jordan Searle <32457721+JordanSearle@users.noreply.github.com>
2019-07-13 21:44:42 +0100

    Started to test the functionality
    The system that has been created is being tested. to start this a new Blog template page has been created that  will allow me to view all the data needed for the blog, this also means that in the very near future I can start to properly use it as a Blog and post updates of it's own creation.
    
    Note: All classes and page will need to be reformated in the future. removing useless or redundant code.

--- class/post.php
+++ class/post.php
@@ -96,5 +96,45 @@ class posts
           echo "Error: " . $sql . "<br>" . $conn->error;
       }
     }
+
+
+    function return_blog_Post(){
+
+      $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+      if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+      }
+
+      $sql = "SELECT post_Date, CASE  WHEN LENGTH(postBody) >= 300 THEN CONCAT(LEFT(postBody,300),' . . .') ELSE postBody END AS postBody, postTitle, post_group.groupTitle FROM post inner join post_group where post.groupID = post_group.groupID";
+      $result = $conn->query($sql);
+
+      if ($result->num_rows > 0) {
+          // output data of each row
+          while($row = $result->fetch_assoc()) {
+            echo ' <div class="w3-card-4 w3-margin w3-white">
+                <img src="https://www.w3schools.com/w3images/woods.jpg" alt="Nature" style="width:100%">
+                <div class="w3-container">
+                  <h3><b>'.$row["postTitle"].'</b></h3>
+                  <h5>'. $row["groupTitle"].'<span class="w3-opacity"> '. date("m/d/Y", strtotime($row["post_Date"]))  .'</span></h5>
+                </div>
+
+                <div class="w3-container">
+                  <p>'.$row["postBody"].'</p>
+                  <div class="w3-row">
+                    <div class="w3-col m8 s12">
+                      <p><button class="w3-button w3-padding-large w3-white w3-border"><b>READ MORE »</b></button></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <hr>';
+          }
+      } else {
+          echo "0 results";
+      }
+      $conn->close();
+
+
+    }
 }
 ?>
